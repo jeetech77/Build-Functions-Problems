1c8ec19e23788de837cbdc880fe5e95e1aa7dc4e
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-06-20 21:36:23 +0530

    Fix : Min value

--- Easy/4thMinNum.js
+++ Easy/4thMinNum.js
@@ -1,5 +1,5 @@
 const readlineSync = require('readline-sync');
-function minValue(...values) {
+function minValue(...values) {    // Using rest parameter
     return values.reduce((firstVal, secVal) => {
         let minValue = firstVal;
         if (minValue<secVal) {
@@ -12,9 +12,9 @@ function minValue(...values) {
 }
 
 let userNumbers = readlineSync.question("Enter all the numbers seprating by comma...\n");
-let listOfNumbers = userNumbers.split(',');
+let listOfNumbers = userNumbers.split(',');   // Spliting them to get the numbers
 for (let index = 0; index < listOfNumbers.length; index++) {
-    let temp=parseInt(listOfNumbers[index]);
+    let temp=parseInt(listOfNumbers[index]);    // Converting string to numbers
     listOfNumbers[index]=temp
 }
-console.log(listOfNumbers);
+console.log(minValue(...listOfNumbers));    // Using spread operator to pass an array to the function.
