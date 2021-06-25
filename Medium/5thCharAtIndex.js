54b99d610fed49c09410918cf1fb6810aec7e016
Ranjeet Singh <epiceye77@gmail.com>
2021-06-25 16:54:43 +0530

    Solved Medium 5th

---
+++ Medium/5thCharAtIndex.js
@@ -0,0 +1,7 @@
+let string = "Neogcamp is love";
+function charAtIndex(str, index) {
+    return str[index];
+}
+console.log(`Character presented at the given index is : ${charAtIndex(string,4)}`)
+console.log(`Character presented at the given index is : ${charAtIndex(string,12)}`)
+console.log(`Character presented at the given index is : ${charAtIndex(string,10)}`)
